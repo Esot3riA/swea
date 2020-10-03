75384e8cf80001042ce09f0f3f82fc111efda039
Esot3riA <esot3ria@gmail.com>
2020-10-03 19:31:33 +0900

    Modify 5254 problem

--- advanced/5254_trie.py
+++ advanced/5254_trie.py
@@ -1,55 +1,46 @@
-from collections import deque
 import sys
 sys.stdin = open("input.txt", 'r')
 
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.full_str = None
-        self.children = {}
+def long_comp_pref(word1,word2):
+    word1_len=len(word1)
+    word2_len=len(word2)
+    min_len=min(word1_len,word2_len)
 
 
-class Trie:
-    def __init__(self):
-        self.head = Node(None)
-
-    def insert(self, str):
-        curr_node = self.head
-        for char in str:
-            if char not in curr_node.children:
-                curr_node.children[char] = Node(char)
-            curr_node = curr_node.children[char]
-        curr_node.full_str = str
-
-    def find_substr(self, n):
-        # DFS
-        substr_count = 0
-        queue = deque()
-        queue.append(self.head)
-        while queue:
-            cur_node = queue.pop()
-            if cur_node.data == "$":
-                substr_count += 1
-                if substr_count == n:
-                    return cur_node.full_str
-
-            node_keys = list(cur_node.children.keys())
-            node_keys.sort(reverse=True)
-            for key in node_keys:
-                queue.append(cur_node.children[key])
+    for cnt in range(min_len):
+        if word1[:cnt+1]!= word2[:cnt+1]:
+            break
+    return cnt
 
 
-testCase = int(input())
-for case in range(1, testCase + 1):
-    N, input_str = input().split()
-    str_len = len(input_str)
+def find_n_th_partial_word(words,n,max_len):
+    i=0
+    words_len=len(words)
+    lcp=0
 
-    trie = Trie()
-    for i in range(str_len):
-        for j in range(i + 1, str_len + 1):
-            substr = input_str[i:j] + "$"
-            trie.insert(substr)
+    while(i<words_len):
+        cur_idx,cur_suf=words[i]
+        candidates_num=max_len-cur_idx
+        # 이전 suffix의 부분문자열과 중복되지 않는 현재 suffix의 부분 문자열의 실제 갯수 = candidates_num - lcp
+        if candidates_num-lcp >= n:
+            # suffix의 prefix는 길이가 짧을 수록 사전순으로 정렬시 먼저 온다.
+            # 중복된 경우를 제외할 경우 n번째 부분 문자열은 n+lcp 번째 prefix이다.
+            return cur_suf, len(cur_suf[:n+lcp])
+        else:
+            n-= (candidates_num-lcp)
+            if i!=(words_len-1):
+                lcp=long_comp_pref(cur_suf,words[i+1][1]) #cur_suf=words[i][1]
+            i+=1
+    return 0,0
 
-    result_str = trie.find_substr(int(N))
-    print("#{} {} {}".format(case, result_str[0], len(result_str) - 1))
+
+for t in range(1, int(input())+1):
+    str_N,word=input().split()
+    word_len=len(word)
+
+    # ordered_suffixes의 요소들은 서로 구분가능 하다 (길이가 모두 다르기 때문).
+    unordered_suffixes=[(i,word[i:]) for i in range(word_len)]
+    ordered_suffixes=sorted(unordered_suffixes,key = lambda elements: elements[1])
+    partial_word, its_len=find_n_th_partial_word(ordered_suffixes,int(str_N),word_len)
+    print('#{} {} {}'.format(t,partial_word[0],its_len))
